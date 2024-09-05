40bd5bd4c4f807e446784f09e96acf8808f62c30
Saleh Mirshafiee <seyed82saleh@gmail.com>
2024-09-05 13:09:31 +0330

    Handling style of  heart button

--- rjs-book-app/src/components/BookCard.jsx
+++ rjs-book-app/src/components/BookCard.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { GoHeartFill } from "react-icons/go";
 
 function BookCard({ data: { title, author, image, language, pages } }) {
+  const [like, setlike] = useState(false);
+
+  const likeHandler = () => setlike((like) => !like);
+
   return (
     <div>
       <img src={image} alt={title} />
@@ -12,8 +17,8 @@ function BookCard({ data: { title, author, image, language, pages } }) {
           <span>{pages} pages</span>
         </div>
       </div>
-      <button>
-        <GoHeartFill />
+      <button onClick={likeHandler}>
+        <GoHeartFill color={like ? "#efefef" : "#142f40"} fontSize="1.8rem" />
       </button>
     </div>
   );
